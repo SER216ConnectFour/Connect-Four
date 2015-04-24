1b03bd3c9d560931f31c3092ee2b9f434a608043
sjud9227 <SWD@Scotts-MacBook-Pro-2.local>
2015-04-24 16:47:41 -0700

    Board.java

---
+++ connect/four/player/ComputerPlayerTest.java
@@ -0,0 +1,34 @@
+package connect.four.player;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ComputerPlayerTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		
+	}
+
+}
